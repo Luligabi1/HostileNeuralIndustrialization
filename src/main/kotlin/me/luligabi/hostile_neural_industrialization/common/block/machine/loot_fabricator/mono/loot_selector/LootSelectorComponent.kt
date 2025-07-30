6b88c946ba260cff2138539b8f9beb8142fb5f01
Luligabi1 <luligabi111@gmail.com>
2025-07-30 19:45:51 -0300

    missing file oops

--- src/main/kotlin/me/luligabi/hostile_neural_industrialization/common/block/machine/loot_fabricator/mono/loot_selector/LootSelectorComponent.kt
+++ src/main/kotlin/me/luligabi/hostile_neural_industrialization/common/block/machine/loot_fabricator/mono/loot_selector/LootSelectorComponent.kt
@@ -1,16 +1,32 @@
 package me.luligabi.hostile_neural_industrialization.common.block.machine.loot_fabricator.mono.loot_selector
 
+import aztech.modern_industrialization.inventory.ChangeListener
+import aztech.modern_industrialization.inventory.ConfigurableItemStack
 import aztech.modern_industrialization.machines.IComponent
+import me.luligabi.hostile_neural_industrialization.common.block.machine.loot_fabricator.mono.MonoLootFabricatorBlockEntity
+import me.luligabi.hostile_neural_industrialization.common.misc.network.RefreshLootListPacket
 import net.minecraft.core.HolderLookup
 import net.minecraft.nbt.CompoundTag
 import net.minecraft.resources.ResourceLocation
+import net.minecraft.server.level.ServerLevel
 
-class LootSelectorComponent: IComponent.ServerOnly {
+
+class LootSelectorComponent(val blockEntity: () -> MonoLootFabricatorBlockEntity): IComponent.ServerOnly {
 
     var selectedLootId: ResourceLocation? = null
+    lateinit var inputStackListener: InputStackListener
+
+    fun initInputStackListener(triggerOnChange: Boolean = true) {
+        val input = blockEntity().inventory.itemInputs[0]
+
+        val listener = InputStackListener(input, blockEntity)
+        listener.listenAll(listOf(input), null)
+        if (triggerOnChange) listener.onChange()
+        inputStackListener = listener
+    }
+
 
     override fun writeNbt(tag: CompoundTag, registries: HolderLookup.Provider) {
-
         if (selectedLootId == null) {
             tag.remove(SELECTED_LOOT_KEY)
             return
@@ -32,6 +48,28 @@ class LootSelectorComponent: IComponent.ServerOnly {
     companion object {
 
         const val SELECTED_LOOT_KEY = "selectedLootId"
+
+    }
+
+    inner class InputStackListener(
+        private val stack: ConfigurableItemStack,
+        private val blockEntity: () -> MonoLootFabricatorBlockEntity
+    ) : ChangeListener() {
+
+        private var lastVariant = stack.variant
+
+        public override fun onChange() {
+            if (blockEntity().level?.isClientSide == true) return
+
+            if (!stack.variant.equals(lastVariant)) {
+                (blockEntity().level as? ServerLevel)?.let { // FIXME this bullshit
+                    RefreshLootListPacket.INSTANCE.broadcastToClients(it, blockEntity().blockPos, 8.0)
+                }
+            }
+            lastVariant = stack.variant
+        }
+
+        override fun isValid(token: Any?) = true
 
     }
 
