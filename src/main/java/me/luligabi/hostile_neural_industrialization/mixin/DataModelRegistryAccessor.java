a411d4739d6e1293b0674f010d8952f9bd00bbbd
Luiz Feikes <33578169+Luligabi1@users.noreply.github.com>
2025-08-01 20:52:27 -0300

    Update to HNN 6.3.0

--- src/main/java/me/luligabi/hostile_neural_industrialization/mixin/DataModelRegistryAccessor.java
+++ src/main/java/me/luligabi/hostile_neural_industrialization/mixin/DataModelRegistryAccessor.java
@@ -1,20 +1,16 @@
 package me.luligabi.hostile_neural_industrialization.mixin;
 
+import com.google.common.collect.Multimap;
 import dev.shadowsoffire.hostilenetworks.data.DataModel;
 import dev.shadowsoffire.hostilenetworks.data.DataModelRegistry;
-import dev.shadowsoffire.hostilenetworks.data.ModelTier;
-import dev.shadowsoffire.hostilenetworks.data.ModelTierRegistry;
 import net.minecraft.world.entity.EntityType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
-
-import java.util.LinkedList;
-import java.util.Map;
 
 @Mixin(value = DataModelRegistry.class, remap = false)
 public interface DataModelRegistryAccessor {
 
     @Accessor("modelsByType")
-    Map<EntityType<?>, DataModel> getModelsByType();
+    Multimap<EntityType<?>, DataModel> getModelsByType();
 
 }
