930517f846a6d07d687fb5c1b591aa7a0f8e3f14
Luiz Feikes <33578169+Luligabi1@users.noreply.github.com>
2025-09-03 22:25:00 -0300

    Temp fix for multiblock GUI names, bump to 1.0.4

---
+++ src/main/java/me/luligabi/hostile_neural_industrialization/mixin/MachineBlockEntityMixin.java
@@ -0,0 +1,25 @@
+package me.luligabi.hostile_neural_industrialization.mixin;
+
+import aztech.modern_industrialization.machines.MachineBlockEntity;
+import me.luligabi.hostile_neural_industrialization.common.HNI;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.network.chat.Component;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = MachineBlockEntity.class, remap = false)
+public abstract class MachineBlockEntityMixin {
+
+    @Inject(method = "getDisplayName", at = @At("HEAD"), cancellable = true)
+    private void getDisplayName(CallbackInfoReturnable<Component> callback) {
+        MachineBlockEntity machine = (MachineBlockEntity) (Object) this;
+
+        var blockId = BuiltInRegistries.BLOCK.getKey(machine.getBlockState().getBlock());
+        if (blockId.getNamespace().equals(HNI.ID)) {
+            callback.setReturnValue(machine.getBlockState().getBlock().getName());
+        }
+    }
+
+}
